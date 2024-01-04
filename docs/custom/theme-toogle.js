d917de9cb149edea9ecfbc80c1a40b885fb58ce7
VAMSI KARUTURI <karuturi-sai-naga.vamsi-krishna@siemens.com>
2024-01-04 19:38:03 +0530

    joins

--- docs/custom/theme-toogle.js
+++ docs/custom/theme-toogle.js
@@ -1,24 +1,19 @@
-// const darkModeToggle = document.getElementById("dark-mode-toggle");
-// const body = document.body;
-//
-// // Check for dark mode preference in local storage
-// const darkModePreference = localStorage.getItem("dark-mode");
-// if (darkModePreference === "disabled") {
-//     body.classList.remove("dark-mode");
-//     localStorage.setItem("dark-mode", "disabled");
-// } else {
-//     // Enable dark mode by default
-//     body.classList.add("dark-mode");
-//     localStorage.setItem("dark-mode", "enabled");
-// }
-//
-// // Toggle dark mode
-// darkModeToggle.addEventListener("click", () => {
-//     if (body.classList.contains("dark-mode")) {
-//         body.classList.remove("dark-mode");
-//         localStorage.setItem("dark-mode", "disabled");
-//     } else {
-//         body.classList.add("dark-mode");
-//         localStorage.setItem("dark-mode", "enabled");
-//     }
-// });
+document.addEventListener("DOMContentLoaded", function() {
+    const darkModeToggle = document.getElementById("dark-mode-toggle");
+    const body = document.body;
+
+    // Always enable dark mode by default
+    body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "enabled");
+
+    // Toggle dark mode
+    darkModeToggle.addEventListener("click", () => {
+        if (body.classList.contains("dark-mode")) {
+            body.classList.remove("dark-mode");
+            localStorage.setItem("dark-mode", "disabled");
+        } else {
+            body.classList.add("dark-mode");
+            localStorage.setItem("dark-mode", "enabled");
+        }
+    });
+});
